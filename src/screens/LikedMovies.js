e630a930e59998c112d02bf91fb72a05de07a0f3
CozumIntern <37351493+Melihomac@users.noreply.github.com>
2023-12-20 11:20:34 +0300

    Fix version problems

--- src/screens/LikedMovies.js
+++ src/screens/LikedMovies.js
@@ -1,5 +1,6 @@
-import {getDatabase, ref, onValue} from '@react-native-firebase/database';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {database} from '../../FirebaseConfig';
+import {resolve} from 'path';
 
 export const likedMovies = async dataDetail => {
   try {
@@ -8,12 +9,18 @@ export const likedMovies = async dataDetail => {
 
     await AsyncStorage.setItem('likedMovie', dataDetail?.original_title);
 
-    const dbRef = ref(getDatabase(), 'Film');
-    onValue(dbRef, snapshot => {
-      const data = snapshot.val();
-      console.log('Veritabanındaki güncel veri: ', data);
-    });
-
+    let key;
+    key = database().ref('/movies').push.key;
+    console.log(key);
+    database
+      .ref('movies')
+      .update(result)
+      .then(snapshot => {
+        resolve(snapshot);
+      })
+      .catch(err => {
+        console.log(err);
+      });
     return result, true;
   } catch (error) {
     console.error('Error saving liked movie:', error);
