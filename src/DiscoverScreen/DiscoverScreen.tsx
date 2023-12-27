07d5fbce04227efc0a3c30f11e347f477f0582f1
CozumIntern <37351493+Melihomac@users.noreply.github.com>
2023-12-27 11:22:57 +0300

    Add bardAI textgenerator

--- src/DiscoverScreen/DiscoverScreen.tsx
+++ src/DiscoverScreen/DiscoverScreen.tsx
@@ -1,33 +1,44 @@
 import React, {useState} from 'react';
 import {View, TextInput, Button, Text} from 'react-native';
 import axios from 'axios';
+import {GoogleGenerativeAI} from '@google/generative-ai';
 
 const DiscoverScreen = () => {
   const [inputText, setInputText] = useState('');
   const [outputText, setOutputText] = useState('');
-
+  const apiKey = 'AIzaSyCdqvTfPTluwjTN60tIsIKkWbPccXzMdhE';
+  const genAI = new GoogleGenerativeAI(apiKey);
   const sendRequest = async () => {
     try {
-      const response = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
-        {
-          model: 'gpt-3.5-turbo-1106',
-          messages: [
-            {role: 'system', content: 'You are a helpful assistant.'},
-            {role: 'user', content: inputText},
-          ],
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization:
-              'Bearer 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',
-          },
-        },
-      );
+      const model = genAI.getGenerativeModel({model: 'gemini-pro'});
+      const prompt = inputText;
+      setInputText(prompt);
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+      setOutputText(text);
 
-      const botReply = response.data.choices[0].message.content;
-      setOutputText(botReply);
+      // const response = await axios.post(
+      //   'https://api.openai.com/v1/chat/completions',
+      //   {
+      //     model: 'gpt-3.5-turbo-1106',
+      //     messages: [
+      //       {role: 'system', content: 'You are a helpful assistant.'},
+      //       {role: 'user', content: inputText},
+      //     ],
+      //   },
+      //   {
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //       Authorization:
+      //         'Bearer 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',
+      //     },
+      //   },
+      // );
+
+      // const botReply = response.data.choices[0].message.content;
+      // setOutputText(botReply);
     } catch (error) {
       console.error('Error sending message:', error);
     }
