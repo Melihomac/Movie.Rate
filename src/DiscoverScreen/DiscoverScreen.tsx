ffed2f3ca85006e9f6eab4219bf7da55e3305732
CozumIntern <37351493+Melihomac@users.noreply.github.com>
2023-12-27 20:23:58 +0300

    Add bardAI and Add styling

--- src/DiscoverScreen/DiscoverScreen.tsx
+++ src/DiscoverScreen/DiscoverScreen.tsx
@@ -1,60 +1,129 @@
-import React, {useState} from 'react';
-import {View, TextInput, Button, Text} from 'react-native';
-import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import {GoogleGenerativeAI} from '@google/generative-ai';
+import {firebase} from '../../FirebaseConfig';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ScrollView} from 'react-native-gesture-handler';
+
+const readDataFromFirestore = async collection => {
+  try {
+    const ref = firebase.firestore().collection(collection);
+    const response = await ref.get();
+    return response.docs.map(doc => doc.data());
+  } catch (error) {
+    console.error('Firestore veri çekme hatası:', error);
+    throw error;
+  }
+};
 
 const DiscoverScreen = () => {
-  const [inputText, setInputText] = useState('');
   const [outputText, setOutputText] = useState('');
+  const [movieNames, setMovieNames] = useState([]);
+  const [movieNameList, setMovieNameList] = useState('');
+
   const apiKey = 'AIzaSyCdqvTfPTluwjTN60tIsIKkWbPccXzMdhE';
   const genAI = new GoogleGenerativeAI(apiKey);
+
   const sendRequest = async () => {
     try {
+      console.log('Filmler ' + movieNames.join(', '));
+      const prompt = `Give me a movie similar to these movies: ${movieNames.join(
+        ', ',
+      )}.`;
       const model = genAI.getGenerativeModel({model: 'gemini-pro'});
-      const prompt = inputText;
-      setInputText(prompt);
       const result = await model.generateContent(prompt);
       const response = await result.response;
       const text = response.text();
       console.log(text);
       setOutputText(text);
-
-      // const response = await axios.post(
-      //   'https://api.openai.com/v1/chat/completions',
-      //   {
-      //     model: 'gpt-3.5-turbo-1106',
-      //     messages: [
-      //       {role: 'system', content: 'You are a helpful assistant.'},
-      //       {role: 'user', content: inputText},
-      //     ],
-      //   },
-      //   {
-      //     headers: {
-      //       'Content-Type': 'application/json',
-      //       Authorization:
-      //         'Bearer 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',
-      //     },
-      //   },
-      // );
-
-      // const botReply = response.data.choices[0].message.content;
-      // setOutputText(botReply);
     } catch (error) {
       console.error('Error sending message:', error);
     }
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const docs = await readDataFromFirestore('movies');
+        const names = docs.map(element => element?.name);
+        setMovieNames(names);
+        setMovieNameList(names.join(',\n'));
+      } catch (error) {
+        console.error('Veri getirme hatası:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View>
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: 10}}
-        onChangeText={text => setInputText(text)}
-        value={inputText}
-      />
-      <Button title="Send" onPress={sendRequest} />
-      <Text>{outputText}</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <SafeAreaView style={styles.safeArea}>
+          <View>
+            <View>
+              <Text style={styles.favMovieHeader}>Your Favourite Movies</Text>
+              <Text style={styles.favMovieName}>{movieNameList}</Text>
+            </View>
+            <TouchableOpacity style={styles.buttonStyle} onPress={sendRequest}>
+              <Text style={styles.buttonTextStyle}>Generate AI</Text>
+            </TouchableOpacity>
+            <View>
+              <Text style={styles.textStyle}>{outputText}</Text>
+            </View>
+          </View>
+        </SafeAreaView>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  safeArea: {
+    flex: 1,
+  },
+  buttonStyle: {
+    width: '90%',
+    borderColor: '#A20E0E',
+    borderWidth: 2,
+    padding: 5,
+    marginLeft: 15,
+    borderRadius: 15,
+    zIndex: 3,
+    marginTop: 15,
+  },
+  buttonTextStyle: {
+    fontSize: 25,
+    fontFamily: 'Arial',
+    textAlign: 'center',
+  },
+  textStyle: {
+    fontSize: 15,
+    fontFamily: 'Arial',
+    padding: 20,
+    textAlign: 'left',
+    lineHeight: 25,
+  },
+  favMovieHeader: {
+    fontSize: 30,
+    padding: 15,
+    borderWidth: 2,
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 5,
+    textAlign: 'center',
+    borderRadius: 5,
+    borderColor: '#A20E0E',
+  },
+  favMovieName: {
+    padding: 15,
+    lineHeight: 25,
+    fontSize: 15,
+  },
+});
 
 export default DiscoverScreen;
